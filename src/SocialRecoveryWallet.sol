b6fb365b2713210ad7f467b1a4c543f40c0bc747
Socarde Paul <84129217+DRIVENpol@users.noreply.github.com>
2023-11-15 02:24:39 +0200

    erc721/1155/165 + Receiver fallback

--- src/SocialRecoveryWallet.sol
+++ src/SocialRecoveryWallet.sol
@@ -3,7 +3,16 @@ pragma solidity ^0.8.19;
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SocialRecoveryWallet is Ownable {
+// Imports to be able to manipulate different types of tokens
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import { IERC1155 } from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+
+// Token receiver
+import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import {IERC1155Receiver} from "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
+
+contract SocialRecoveryWallet is Ownable, IERC721Receiver, IERC1155Receiver {
 
     uint256 constant MAX_MEMBERS = 10;
     uint256 constant MAX_FUTURE_ADDRESSES = 3;
@@ -39,7 +48,7 @@ contract SocialRecoveryWallet is Ownable {
             revert TooManyFutureAddresses(_len);
         }
 
-        for (uint256 i = 0; i < len;) {
+        for (uint256 i = 0; i < _len;) {
 
             if(!_isValidAddress(_futureAddresses[i])) {
                 revert InvalidAddress(_futureAddresses[i]);
@@ -170,6 +179,41 @@ contract SocialRecoveryWallet is Ownable {
                 ++i;
             }
         }
+    }
+
+    // ================ ACT AS A RECEIVER ================
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external returns (bytes4){
+        return this.onERC721Received.selector;
+    }
+
+    function onERC1155Received(
+        address operator,
+        address from,
+        uint256 id,
+        uint256 value,
+        bytes calldata data
+    ) external returns(bytes4) {
+        return this.onERC1155Received.selector;
+    }
+
+    function onERC1155BatchReceived(
+        address operator,
+        address from,
+        uint256[] calldata ids,
+        uint256[] calldata values,
+        bytes calldata data
+    ) external returns (bytes4) {
+        return this.onERC1155BatchReceived.selector;
+    }
+
+    function supportsInterface(bytes4 interfaceId) external view returns (bool) {
+        return interfaceId == type(IERC721Receiver).interfaceId || interfaceId == type(IERC1155Receiver).interfaceId;
     }
 
     // @dev Function to validate an address
