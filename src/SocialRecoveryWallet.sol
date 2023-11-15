7a5f623819f2f740e2775a974487c729288d1f9a
Socarde Paul <84129217+DRIVENpol@users.noreply.github.com>
2023-11-15 02:02:32 +0200

    Transaction management + Custom Errors

--- src/SocialRecoveryWallet.sol
+++ src/SocialRecoveryWallet.sol
@@ -5,14 +5,45 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SocialRecoveryWallet is Ownable {
 
+    uint256 constant MAX_MEMBERS = 10;
+
+    // Transaction count
+    uint256 public transactionCount;
+
+    // No. of required signers
+    uint256 public requiredSigners;
+
     // Family members
     address[] public familyMembers;
+
+    // Transaction struct
+    struct Transaction {
+        address target;
+        uint256 value;
+        uint256 signedBy;
+        bytes data;
+        bool executed;
+    }
 
     // Already a family member
     mapping(address => bool) public isFamilyMember;
 
+    // Approved target contracts
+    mapping(address => bool) public approvedTargets;
+
+    // Transactions
+    mapping(uint256 => Transaction) public transactions;
+
+    // Custom errors
+    error NotFamilyMember(address wallet);
+    error InvalidAddress(address wallet);
+    error InvalidMember(address member, bool status);
+    error TooManyMembers(uint256 count);
+
     // Constructor
-    constructor() Ownable(msg.sender) {}
+    constructor() Ownable(msg.sender) {
+        approvedTargets[address(this)] = true;
+    }
 
     // Receive Ether
     receive() external payable {}
@@ -20,22 +51,31 @@ contract SocialRecoveryWallet is Ownable {
     // Modifier - Only family members
     modifier onlyFamilyMembers() {
         if(!isFamilyMember[msg.sender]) {
-            revert ('Not a family member');
+            revert NotFamilyMember(msg.sender);
         }
 
         _;
     }
 
+    // ================ DEFAULT ACTIONS ================
+
     // @dev Function to add family members
+    // @param members Array of family members
     function addFamilyMembers(address[] calldata members) external onlyOwner {
-        for (uint256 i = 0; i < members.length;) {
+        if(members.length > MAX_MEMBERS) {
+            revert TooManyMembers(members.length);
+        }
+
+        uint256 _len = members.length;
+
+        for (uint256 i = 0; i < _len;) {
             
             if(members[i] == address(0) || members[i] == address(0xdead)) {
-                revert ('Invalid address');
+                revert InvalidAddress(members[i]);
             }
 
             if(isFamilyMember[members[i]]) {
-                revert ('Already a family member');
+                revert InvalidMember(members[i], true);
             }
 
             isFamilyMember[members[i]] = true;
